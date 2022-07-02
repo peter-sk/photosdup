47ee4bb99a1aac7dc157163a2162caabdcde1abd
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2022-07-02 18:49:52 +0200

    make it installable

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import pathlib
+
+base_dir = pathlib.Path(__file__).parent
+
+with open(base_dir / "README.md") as f:
+    long_description = f.read()
+
+setup(
+  name = 'photosdup',
+  packages = ['photosdup'],   
+  version = '1.0',
+  license='MIT',        
+  description = 'Photos Duplicate finder - searches for duplicate or similar images in a Mac Photos library.', 
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Peter Schneider-Kamp',
+  author_email = 'petersk@imada.sdu.dk', 
+  url = 'https://github.com/peter-sk/photosdup', 
+  keywords = ['mac', 'photos', 'duplicate', 'finder'],  
+  install_requires=[
+          'scipy',
+          'numpy',
+          'opencv-python',
+          'photoscript',
+          'scikit-image',
+          'PySimpleGUI'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',    
+    'Programming Language :: Python :: 3.9'
+  ]
+)
