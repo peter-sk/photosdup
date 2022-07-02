98857e82e3f6ee43a22956447a1b3d4825703a16
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2022-07-02 19:33:32 +0200

    requirements

--- setup.py
+++ setup.py
@@ -24,7 +24,9 @@ setup(
           'opencv-python',
           'photoscript',
           'scikit-image',
-          'PySimpleGUI'
+          'PySimpleGUI',
+          'tqdm',
+          'matplotlib'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
